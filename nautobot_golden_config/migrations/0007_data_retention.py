3e2e23dbd90eeeb9b2b9a8f6cb0f0dcd26834829
h4ndzdatm0ld <hugotinoco@icloud.com>
2021-12-07 19:37:27 +0000

    multi-stage-data-retention

--- nautobot_golden_config/migrations/0007_data_retention.py
+++ nautobot_golden_config/migrations/0007_data_retention.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-"""This file is meant to take the current ForeginKey relationship assosciated to
-a single repository object from GoldenConfigSetting backup_repo and intended_repo
-attribute and populate a temporary many_to_many attribute."""
-
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from nautobot_golden_config.models import GoldenConfigSetting
 
 
 def convert_many_repos(apps, schema_editor):
@@ -14,16 +9,19 @@ def convert_many_repos(apps, schema_editor):
     Add the current `backup_repository` and `intended_repository` objects
     to the `many_to_many` additional intermediary attritbute to retain data.`
     """
-    settings_obj = GoldenConfigSetting.objects.first()
+    GoldenConfigSetting = apps.get_model("nautobot_golden_config", "GoldenConfigSetting")
 
-    settings_obj.backup_repositories.add(settings_obj.backup_repository)
-    settings_obj.intended_repositories.add(settings_obj.intended_repository)
+    settings_obj = GoldenConfigSetting.objects.first()
+    if settings_obj.backup_repository:
+        settings_obj.backup_repositories.add(settings_obj.backup_repository)
+    if settings_obj.intended_repository:
+        settings_obj.intended_repositories.add(settings_obj.intended_repository)
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("nautobot_golden_config", "0006_multiple_repo_support"),
+        ("nautobot_golden_config", "0006_temp_field"),
     ]
 
     operations = [
--- nautobot_golden_config/migrations/0009_data_retention_final.py
+++ nautobot_golden_config/migrations/0009_data_retention_final.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
-"""This file is meant to take the current ForeginKey relationship assosciated to
-a single repository object from GoldenConfigSetting backup_repo and intended_repo
-attribute and populate a temporary many_to_many attribute."""
-
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from nautobot_golden_config.models import GoldenConfigSetting
 
 
 def convert_many_repos(apps, schema_editor):
     """
-    Add the current `backup_repositories` and `intended_repositories` objects
-    to the `many_to_many` updated fields backup/intended.`
+    Add the current `backup_repository` and `intended_repository` objects
+    to the `many_to_many` additional intermediary attritbute to retain data.`
     """
-    settings_obj = GoldenConfigSetting.objects.first()
+    GoldenConfigSetting = apps.get_model("nautobot_golden_config", "GoldenConfigSetting")
 
-    settings_obj.backup_repository.add(settings_obj.backup_repositories.all())
-    settings_obj.intended_repositoy.add(settings_obj.intended_repositories.all())
+    settings_obj = GoldenConfigSetting.objects.first()
+    if settings_obj.backup_repositories.all():
+        [settings_obj.backup_repository.add(backup_repo) for backup_repo in settings_obj.backup_repositories.all()]
+    if settings_obj.intended_repositories.all():
+        [
+            settings_obj.intended_repository.add(intended_repo)
+            for intended_repo in settings_obj.intended_repositories.all()
+        ]
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("nautobot_golden_config", "0008_backup_intended_to_many"),
+        ("nautobot_golden_config", "0008_convert_many"),
     ]
 
     operations = [

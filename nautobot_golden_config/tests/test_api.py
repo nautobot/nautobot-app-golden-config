65fb397092b23c59899c43c17ac029d48e5db211
Mitch Zubarev-Foxworth <mitchell.foxworth@networktocode.com>
2021-12-23 23:33:19 +0000

    new tests to cover PUTs and POSTs to GCSettings object

--- nautobot_golden_config/tests/test_api.py
+++ nautobot_golden_config/tests/test_api.py
@@ -1,12 +1,15 @@
 """Unit tests for nautobot_golden_config."""
+from copy import deepcopy
 from django.contrib.auth import get_user_model
 
 from django.urls import reverse
 from rest_framework import status
 
 from nautobot.utilities.testing import APITestCase
+from nautobot.extras.models import GitRepository
+from nautobot_golden_config.models import GoldenConfigSetting
 
-from .conftest import create_device, create_feature_rule_json, create_config_compliance
+from .conftest import create_device, create_feature_rule_json, create_config_compliance, create_git_repos
 
 
 User = get_user_model()
@@ -81,3 +84,342 @@ class GoldenConfigAPITest(APITestCase):  # pylint: disable=too-many-ancestors
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertFalse(response.data["compliance"])
+
+
+class GoldenConfigSettingsAPITest(APITestCase):
+    """Verify that the combination of values in a GoldenConfigSettings object POST are valid."""
+
+    def setUp(self):
+        """Create a superuser and token for API calls."""
+        super().setUp()
+        create_git_repos()
+        self.add_permissions("nautobot_golden_config.add_goldenconfigsetting")
+        self.add_permissions("nautobot_golden_config.change_goldenconfigsetting")
+        self.base_view = reverse("plugins-api:nautobot_golden_config-api:goldenconfigsetting-list")
+        self.data = {
+            "tags": [],
+            "computed_fields": {},
+            "custom_fields": {},
+            "_custom_field_data": {},
+            "backup_match_rule": "backup-{{obj.site.region.parent.slug}}",
+            "backup_path_template": "{{obj.site.region.slug}}/{{obj.site.slug}}/{{obj.name}}.cfg",
+            "intended_match_rule": "intended-{{obj.site.region.parent.slug}}",
+            "intended_path_template": "{{obj.site.region.slug}}/{{obj.site.slug}}/{{obj.name}}.cfg",
+            "jinja_path_template": "templates/{{obj.platform.slug}}/{{obj.platform.slug}}_main.j2",
+            "backup_test_connectivity": False,
+            "scope": {"has_primary_ip": "True"},
+            "sot_agg_query": "query ($device_id: ID!) {\r\n  device(id: $device_id) {\r\n    config_context\r\n    device_role {\r\n      name\r\n      slug\r\n    }\r\n    hostname: name\r\n    platform {\r\n      manufacturer {\r\n        name\r\n      }\r\n      name\r\n      napalm_driver\r\n      slug\r\n    }\r\n    primary_ip4 {\r\n      address\r\n      interface {\r\n        name\r\n      }\r\n      id\r\n    }\r\n    site {\r\n      name\r\n      region {\r\n        name\r\n        slug\r\n        parent {\r\n          name\r\n          slug\r\n        }\r\n      }\r\n      slug\r\n    }\r\n  }\r\n}",
+            "jinja_repository": str(GitRepository.objects.get(name="test-jinja-repo-1").id),
+            "backup_repository": [
+                str(GitRepository.objects.get(name="test-backup-repo-1").id),
+                str(GitRepository.objects.get(name="test-backup-repo-2").id),
+            ],
+            "intended_repository": [
+                str(GitRepository.objects.get(name="test-intended-repo-1").id),
+                str(GitRepository.objects.get(name="test-intended-repo-2").id),
+            ],
+        }
+        # Since we enforce a singleton pattern on this model, nuke any auto-created objects.
+        GoldenConfigSetting.objects.all().delete()
+
+    def test_golden_config_settings_create_1backup_with_match_rule(self):
+        """Verify that an invalid POST with an unnecessary match_rule returns an error."""
+        bad_data = deepcopy(self.data)
+        bad_data["backup_repository"] = [str(GitRepository.objects.get(name="test-backup-repo-1").id)]
+        response = self.client.post(
+            self.base_view,
+            data=bad_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            "If you configure only one backup repository, do not enter the backup repository matching rule template.",
+        )
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_create_backup_match_rule_missing(self):
+        """Verify that an invalid POST with a missing backup_match_rule returns an error."""
+        bad_data = deepcopy(self.data)
+        bad_data["backup_match_rule"] = ""
+        response = self.client.post(
+            self.base_view,
+            data=bad_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            "If you specify more than one backup repository, you must provide the backup repository matching rule template.",
+        )
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_create_1intended_with_match_rule(self):
+        """Verify that an invalid POST with an unnecessary match_rule returns an error."""
+        bad_data = deepcopy(self.data)
+        bad_data["intended_repository"] = [str(GitRepository.objects.get(name="test-intended-repo-2").id)]
+        response = self.client.post(
+            self.base_view,
+            data=bad_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            "If you configure only one intended repository, do not enter the intended repository matching rule template.",
+        )
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_create_intended_match_rule_missing(self):
+        """Verify that an invalid POST with a missing intended_match_rule returns an error."""
+        bad_data = deepcopy(self.data)
+        bad_data["intended_match_rule"] = ""
+        response = self.client.post(
+            self.base_view,
+            data=bad_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            "If you specify more than one intended repository, you must provide the intended repository matching rule template.",
+        )
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_create_multiple_problems(self):
+        """Verify that an invalid POST with multiple problems return multiple, correct errors."""
+        bad_data = deepcopy(self.data)
+        bad_data["backup_repository"] = [str(GitRepository.objects.get(name="test-backup-repo-1").id)]
+        bad_data["intended_match_rule"] = ""
+        response = self.client.post(
+            self.base_view,
+            data=bad_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            "If you configure only one backup repository, do not enter the backup repository matching rule template.",
+        )
+        self.assertEqual(
+            response.data["non_field_errors"][1],
+            "If you specify more than one intended repository, you must provide the intended repository matching rule template.",
+        )
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_create_good(self):
+        """Test a POST with good values."""
+        response = self.client.post(
+            self.base_view,
+            data=self.data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(response.data["created"])
+        self.assertTrue(response.data["id"])
+        self.assertEqual(response.data["backup_match_rule"], "backup-{{obj.site.region.parent.slug}}")
+        self.assertEqual(
+            response.data["backup_path_template"], "{{obj.site.region.slug}}/{{obj.site.slug}}/{{obj.name}}.cfg"
+        )
+        self.assertEqual(response.data["intended_match_rule"], "intended-{{obj.site.region.parent.slug}}")
+        self.assertEqual(
+            response.data["intended_path_template"], "{{obj.site.region.slug}}/{{obj.site.slug}}/{{obj.name}}.cfg"
+        )
+        self.assertEqual(
+            response.data["jinja_path_template"], "templates/{{obj.platform.slug}}/{{obj.platform.slug}}_main.j2"
+        )
+        self.assertFalse(response.data["backup_test_connectivity"])
+        self.assertEqual(response.data["scope"], {"has_primary_ip": "True"})
+        self.assertEqual(
+            response.data["sot_agg_query"],
+            "query ($device_id: ID!) {\r\n  device(id: $device_id) {\r\n    config_context\r\n    device_role {\r\n      name\r\n      slug\r\n    }\r\n    hostname: name\r\n    platform {\r\n      manufacturer {\r\n        name\r\n      }\r\n      name\r\n      napalm_driver\r\n      slug\r\n    }\r\n    primary_ip4 {\r\n      address\r\n      interface {\r\n        name\r\n      }\r\n      id\r\n    }\r\n    site {\r\n      name\r\n      region {\r\n        name\r\n        slug\r\n        parent {\r\n          name\r\n          slug\r\n        }\r\n      }\r\n      slug\r\n    }\r\n  }\r\n}",
+        )
+        self.assertEqual(response.data["jinja_repository"], GitRepository.objects.get(name="test-jinja-repo-1").id)
+        self.assertEqual(
+            response.data["backup_repository"],
+            [
+                GitRepository.objects.get(name="test-backup-repo-1").id,
+                GitRepository.objects.get(name="test-backup-repo-2").id,
+            ],
+        )
+        self.assertEqual(
+            response.data["intended_repository"],
+            [
+                GitRepository.objects.get(name="test-intended-repo-1").id,
+                GitRepository.objects.get(name="test-intended-repo-2").id,
+            ],
+        )
+        # Clean up
+        GoldenConfigSetting.objects.all().delete()
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_update_good(self):
+        """Verify a PUT to the valid settings object, with valid but changed values."""
+        response_post = self.client.post(
+            self.base_view,
+            data=self.data,
+            format="json",
+            **self.header,
+        )
+        new_data = deepcopy(self.data)
+        new_data["backup_repository"] = [str(GitRepository.objects.get(name="test-backup-repo-1").id)]
+        new_data["backup_match_rule"] = ""
+        response = self.client.put(
+            f"{self.base_view}{response_post.data['id']}/",
+            data=new_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["backup_match_rule"], "")
+        self.assertEqual(
+            response.data["backup_path_template"], "{{obj.site.region.slug}}/{{obj.site.slug}}/{{obj.name}}.cfg"
+        )
+        self.assertEqual(response.data["intended_match_rule"], "intended-{{obj.site.region.parent.slug}}")
+        self.assertEqual(
+            response.data["intended_path_template"], "{{obj.site.region.slug}}/{{obj.site.slug}}/{{obj.name}}.cfg"
+        )
+        self.assertEqual(
+            response.data["jinja_path_template"], "templates/{{obj.platform.slug}}/{{obj.platform.slug}}_main.j2"
+        )
+        self.assertFalse(response.data["backup_test_connectivity"])
+        self.assertEqual(response.data["scope"], {"has_primary_ip": "True"})
+        self.assertEqual(
+            response.data["sot_agg_query"],
+            "query ($device_id: ID!) {\r\n  device(id: $device_id) {\r\n    config_context\r\n    device_role {\r\n      name\r\n      slug\r\n    }\r\n    hostname: name\r\n    platform {\r\n      manufacturer {\r\n        name\r\n      }\r\n      name\r\n      napalm_driver\r\n      slug\r\n    }\r\n    primary_ip4 {\r\n      address\r\n      interface {\r\n        name\r\n      }\r\n      id\r\n    }\r\n    site {\r\n      name\r\n      region {\r\n        name\r\n        slug\r\n        parent {\r\n          name\r\n          slug\r\n        }\r\n      }\r\n      slug\r\n    }\r\n  }\r\n}",
+        )
+        self.assertEqual(response.data["jinja_repository"], GitRepository.objects.get(name="test-jinja-repo-1").id)
+        self.assertEqual(
+            response.data["backup_repository"],
+            [
+                GitRepository.objects.get(name="test-backup-repo-1").id,
+            ],
+        )
+        self.assertEqual(
+            response.data["intended_repository"],
+            [
+                GitRepository.objects.get(name="test-intended-repo-1").id,
+                GitRepository.objects.get(name="test-intended-repo-2").id,
+            ],
+        )
+        # Clean up
+        GoldenConfigSetting.objects.all().delete()
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_update_1backup_with_match_rule(self):
+        """Verify a PUT to the valid settings object, with an invalid backup repo set, returns a 400."""
+        response_post = self.client.post(
+            self.base_view,
+            data=self.data,
+            format="json",
+            **self.header,
+        )
+        bad_data = deepcopy(self.data)
+        bad_data["backup_repository"] = [str(GitRepository.objects.get(name="test-backup-repo-1").id)]
+        response = self.client.put(
+            f"{self.base_view}{response_post.data['id']}/",
+            data=bad_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            "If you configure only one backup repository, do not enter the backup repository matching rule template.",
+        )
+        # Clean up
+        GoldenConfigSetting.objects.all().delete()
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_update_backup_match_rule_missing(self):
+        """Verify a PUT to the valid settings object, with an invalid backup repo set, returns a 400."""
+        response_post = self.client.post(
+            self.base_view,
+            data=self.data,
+            format="json",
+            **self.header,
+        )
+        bad_data = deepcopy(self.data)
+        bad_data["backup_match_rule"] = ""
+        response = self.client.put(
+            f"{self.base_view}{response_post.data['id']}/",
+            data=bad_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            "If you specify more than one backup repository, you must provide the backup repository matching rule template.",
+        )
+        # Clean up
+        GoldenConfigSetting.objects.all().delete()
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_update_1intended_with_match_rule(self):
+        """Verify a PUT to the valid settings object, with an invalid intended repo set, returns a 400."""
+        response_post = self.client.post(
+            self.base_view,
+            data=self.data,
+            format="json",
+            **self.header,
+        )
+        bad_data = deepcopy(self.data)
+        bad_data["intended_repository"] = [str(GitRepository.objects.get(name="test-intended-repo-1").id)]
+        response = self.client.put(
+            f"{self.base_view}{response_post.data['id']}/",
+            data=bad_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            "If you configure only one intended repository, do not enter the intended repository matching rule template.",
+        )
+        # Clean up
+        GoldenConfigSetting.objects.all().delete()
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_golden_config_settings_update_intended_match_rule_missing(self):
+        """Verify a PUT to the valid settings object, with an invalid intended repo set, returns a 400."""
+        response_post = self.client.post(
+            self.base_view,
+            data=self.data,
+            format="json",
+            **self.header,
+        )
+        bad_data = deepcopy(self.data)
+        bad_data["intended_match_rule"] = ""
+        response = self.client.put(
+            f"{self.base_view}{response_post.data['id']}/",
+            data=bad_data,
+            format="json",
+            **self.header,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["non_field_errors"][0],
+            "If you specify more than one intended repository, you must provide the intended repository matching rule template.",
+        )
+        # Clean up
+        GoldenConfigSetting.objects.all().delete()
+        self.assertEqual(GoldenConfigSetting.objects.all().count(), 0)
+
+    def test_settings_api_clean_up(self):
+        """Transactional custom model, unable to use `get_or_create`.
+
+        Delete all objects created of GitRepository type.
+        """
+        GitRepository.objects.all().delete()
+        self.assertEqual(GitRepository.objects.all().count(), 0)
+
+        # Put back a general GoldenConfigSetting object.
+        global_settings = GoldenConfigSetting.objects.create()
+        global_settings.save()

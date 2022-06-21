47cad96ee42be28d34847de85f48cead98e881b6
Jeremy White <jeremy.white@networktocode.com>
2022-06-21 11:30:32 -0500

    changing to commong version pattern

--- nautobot_golden_config/__init__.py
+++ nautobot_golden_config/__init__.py
@@ -1,6 +1,12 @@
 """Plugin declaration for nautobot_golden_config."""
+# Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
+try:
+    from importlib import metadata
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata
 
-__version__ = "1.0.3"
+__version__ = metadata.version(__name__)
 
 from nautobot.extras.plugins import PluginConfig
 

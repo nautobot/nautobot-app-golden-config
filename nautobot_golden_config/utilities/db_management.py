b16b4c213e5651e86c776a9b7403aa2844d6219d
Uros Bajzelj <ubajze@users.noreply.github.com>
2022-09-23 16:08:53 +0200

    Linting

--- nautobot_golden_config/utilities/db_management.py
+++ nautobot_golden_config/utilities/db_management.py
@@ -4,6 +4,7 @@ from django.db import connections
 
 def close_threaded_db_connections(func):
     """Decorator that clears idle DB connections in thread."""
+
     def inner(*args, **kwargs):
         """Inner function."""
         func(*args, **kwargs)

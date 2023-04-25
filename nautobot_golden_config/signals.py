bba015819414dd77ec3df62c4e89fe9f3a488edf
Jeff Kala <jeff.kala@networktocode.com>
2023-04-25 15:01:36 -0600

    add signal

--- nautobot_golden_config/signals.py
+++ nautobot_golden_config/signals.py
@@ -1,13 +1,13 @@
 """Signal helpers."""
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from nautobot.dcim.models import Platform
 from nautobot_golden_config import models
 
 
-@receiver(pre_save, sender=models.ConfigCompliance)
-def config_compliance_platform_cleanup(sender, instance, **kwargs):
+@receiver(post_save, sender=models.ConfigCompliance)
+def config_compliance_platform_cleanup(sender, instance, **kwargs):  # pylint: disable=unused-argument
     """Signal helper to delete any orphaned ConfigCompliance objects. Caused by device platform changes."""
     cc_wrong_platform = models.ConfigCompliance.objects.exclude(
         device=instance.device, rule__platform=Platform.objects.get(slug=instance.device.platform.slug)

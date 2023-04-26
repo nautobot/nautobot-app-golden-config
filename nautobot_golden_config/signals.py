664b2385be6ab40d740a6a5f9b5cbc67797060cc
Jeff Kala <jeff.kala@networktocode.com>
2023-04-26 14:16:33 -0600

    fix post_save method and add test

--- nautobot_golden_config/signals.py
+++ nautobot_golden_config/signals.py
@@ -9,8 +9,8 @@ from nautobot_golden_config import models
 @receiver(post_save, sender=models.ConfigCompliance)
 def config_compliance_platform_cleanup(sender, instance, **kwargs):  # pylint: disable=unused-argument
     """Signal helper to delete any orphaned ConfigCompliance objects. Caused by device platform changes."""
-    cc_wrong_platform = models.ConfigCompliance.objects.exclude(
-        device=instance.device, rule__platform=Platform.objects.get(slug=instance.device.platform.slug)
+    cc_wrong_platform = models.ConfigCompliance.objects.filter(device=instance.device).filter(
+        rule__platform__in=Platform.objects.exclude(slug=instance.device.platform.slug)
     )
     if cc_wrong_platform.count() > 0:
         cc_wrong_platform.delete()

562f22ec3c548a5fe133a73e3b6d33d6dfe89163
Marek Zbroch <marek.zbroch@networktocode.com>
2023-06-08 12:14:28 +0200

    typo fix

--- nautobot_golden_config/choices.py
+++ nautobot_golden_config/choices.py
@@ -25,21 +25,3 @@ class RemediationTypeChoice(ChoiceSet):
         (TYPE_HIERCONFIG, "HIERCONFIG"),
         (TYPE_CUSTOM, "CUSTOM"),
     )
-
-
-class ConfigPlanTypeChoice(ChoiceSet):
-    """Choiceset used by ConfigPlan."""
-
-    TYPE_FEATURE_INTENDED = "feature_intended"
-    TYPE_FEATURE_MISSING = "feature_missing"
-    TYPE_FEATURE_REMEDIATION = "feature_remediation"
-    TYPE_FULL_REPLACE = "full_replace"
-    TYPE_MANUAL = "manual"
-
-    CHOICES = (
-        (TYPE_FEATURE_INTENDED, "FEATURE_INTENDED"),
-        (TYPE_FEATURE_MISSING, "FEATURE_MISSING"),
-        (TYPE_FEATURE_REMEDIATION, "TYPE_FEATURE_REMEDIATION"),
-        (TYPE_FULL_REPLACE, "FULL_REPLACE"),
-        (TYPE_MANUAL, "MANUAL"),
-    )

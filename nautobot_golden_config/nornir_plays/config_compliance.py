ac5c1b3b391ed2122f5fc7a1b42dfcb8df97d5c1
pato23arg <patricior_villar@hotmail.com>
2023-06-23 11:02:29 +0200

    modified:   nautobot_golden_config/nornir_plays/config_compliance.py

--- nautobot_golden_config/nornir_plays/config_compliance.py
+++ nautobot_golden_config/nornir_plays/config_compliance.py
@@ -18,7 +18,7 @@ from nornir_nautobot.utils.logger import NornirLogger
 from nautobot_plugin_nornir.plugins.inventory.nautobot_orm import NautobotORMInventory
 from nautobot_plugin_nornir.constants import NORNIR_SETTINGS
 
-from nautobot_golden_config.choices import ComplianceRuleTypeChoice
+from nautobot_golden_config.choices import ComplianceRuleConfigTypeChoice
 from nautobot_golden_config.utilities.db_management import close_threaded_db_connections
 from nautobot_golden_config.models import ComplianceRule, ConfigCompliance, GoldenConfigSetting, GoldenConfig
 from nautobot_golden_config.utilities.helper import (
@@ -60,21 +60,21 @@ def get_config_element(rule, config, obj, logger):
        - a configuration section for `CLI` based config types
        - top level JSON key for `JSON` based config types
     """
-    if rule["obj"].config_type == ComplianceRuleTypeChoice.TYPE_JSON:
+    if rule["obj"].config_type == ComplianceRuleConfigTypeChoice.TYPE_JSON:
         config_json = get_json_config(config)
 
         if not config_json:
             logger.log_failure(obj, "Unable to interpret configuration as JSON.")
             raise NornirNautobotException("Unable to interpret configuration as JSON.")
 
-        if rule["obj"].config_to_match:
+        if rule["obj"].match_config:
             config_element = {
-                k: config_json.get(k) for k in rule["obj"].config_to_match.splitlines() if k in config_json
+                k: config_json.get(k) for k in rule["obj"].match_config.splitlines() if k in config_json
             }
         else:
             config_element = config_json
 
-    elif rule["obj"].config_type == ComplianceRuleTypeChoice.TYPE_CLI:
+    elif rule["obj"].config_type == ComplianceRuleConfigTypeChoice.TYPE_CLI:
         if get_platform(obj.platform.slug) not in parser_map.keys():
             logger.log_failure(
                 obj,

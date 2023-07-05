c38904ba36bd9c668a9993ceeb8286704f4522b3
Joe Wesch <joe.wesch@networktocode.com>
2023-07-05 16:07:25 -0500

    Adds signal to create default ConfigPlan statuses

--- nautobot_golden_config/signals.py
+++ nautobot_golden_config/signals.py
@@ -1,9 +1,35 @@
 """Signal helpers."""
-
+from django.apps import apps as global_apps
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from nautobot.dcim.models import Platform
 from nautobot_golden_config import models
+
+
+def post_migrate_create_statuses(sender, apps=global_apps, **kwargs):  # pylint: disable=unused-argument
+    """Callback function for post_migrate() -- create Status records."""
+    Status = apps.get_model("extras", "Status")  # pylint: disable=invalid-name
+    ContentType = apps.get_model("contenttypes", "ContentType")  # pylint: disable=invalid-name
+    for status_config in [
+        {
+            "name": "Accepted",
+            "slug": "accepted",
+            "defaults": {
+                "description": "Config plan is accepted",
+                "color": "4caf50",  # Green
+            },
+        },
+        {
+            "name": "Not Accepted",
+            "slug": "not-accepted",
+            "defaults": {
+                "description": "Config plan is not accepted",
+                "color": "f44336",  # Red
+            },
+        },
+    ]:
+        status, _ = Status.objects.get_or_create(**status_config)
+        status.content_types.add(ContentType.objects.get_for_model(models.ConfigPlan))
 
 
 @receiver(post_save, sender=models.ConfigCompliance)

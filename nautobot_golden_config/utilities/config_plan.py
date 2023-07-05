542dca4a2181f98c544ff6569675dd76043c8ce4
Joe Wesch <joe.wesch@networktocode.com>
2023-07-05 16:06:12 -0500

    Changes functions to return config_set instead

--- nautobot_golden_config/utilities/config_plan.py
+++ nautobot_golden_config/utilities/config_plan.py
@@ -3,67 +3,42 @@ from django.contrib.contenttypes.models import ContentType
 
 from nautobot.dcim.models import Device
 from nautobot.extras.models import Status
+from nautobot.utilities.utils import render_jinja2
 
 from nautobot_golden_config.models import ConfigPlan, ComplianceFeature
 
 # TODO: Make the default Status configurable
-DEFAULT_STATUS = Status.objects.filter(
+CONFIG_PLAN_DEFAULT_STATUS = Status.objects.filter(
     content_types=ContentType.objects.get_for_model(ConfigPlan),
     slug="not-accepted",
 ).first()
 
 
-def generate_config_plan_from_compliance_feature(
-    device: Device,
-    plan_type: str,
-    feature: ComplianceFeature,
-    change_control_id: str = None,
-    status: Status = None,
-):
-    """Generate config plan from config compliance.
+def generate_config_set_from_compliance_feature(device: Device, plan_type: str, feature: ComplianceFeature):
+    """Generate config set from config compliance.
 
     Args:
-        device (Device): Device to generate config plan for.
+        device (Device): Device to generate config set for.
         plan_type (str): The ConfigCompliance attribute to pull from.
-        feature (ComplianceFeature): The feature to generate config plan for.
-        change_control_id (str, optional): The change control ID to use for the config plan.
-        status (Status, optional): The status to use for the config plan.
+        feature (ComplianceFeature): The feature to generate config set for.
     """
     # Grab the config compliance for the feature
     feature_compliance = device.configcompliance_set.filter(rule__feature=feature).first()
-    # If the config compliance exists and has the plan type generated, create the config plan
+    # If the config compliance exists and has the plan type generated, return the config set
     if feature_compliance and hasattr(feature_compliance, plan_type) and getattr(feature_compliance, plan_type):
-        status = status or DEFAULT_STATUS
-        return ConfigPlan.objects.create(
-            plan_type=plan_type,
-            device=device,
-            config_set=getattr(feature_compliance, plan_type),
-            feature=feature,
-            change_control_id=change_control_id,
-            status=status,
-        )
-    return ConfigPlan.objects.none()
+        return getattr(feature_compliance, plan_type)
+    return ""
 
 
-def generate_config_plan_from_manual(
-    device: Device,
-    config_set: str,
-    change_control_id: str = None,
-    status: Status = None,
-):
-    """Generate config plan from manual config set.
+def generate_config_set_from_manual(device: Device, commands: str, context: dict = None):
+    """Generate config set from manual config set.
 
     Args:
-        device (Device): Device to generate config plan for.
-        config_set (str): The config set for the generated config plan.
-        change_control_id (str, optional): The change control ID to use for the config plan.
-        status (Status, optional): The status to use for the config plan.
+        device (Device): Device to generate config set for.
+        commands (str): The commands for the generated config set.
+        context (dict, optional): The context to render the commands with.
     """
-    status = status or DEFAULT_STATUS
-    return ConfigPlan.objects.create(
-        plan_type="manual",
-        device=device,
-        config_set=config_set,
-        change_control_id=change_control_id,
-        status=status,
-    )
+    if context is None:
+        context = {}
+    context.update({"obj": device})
+    return render_jinja2(template_code=commands, context=context)

47d96d040a03d2b67b01274aa040a61eda96065b
Eric Hansen <ericzone.admin@gmail.com>
2023-07-17 12:03:35 -0400

    fix: 🐛 Change default setting for lstrip_blocks to False
    https://github.com/nautobot/nautobot-plugin-golden-config/issues/525

--- development/nautobot_config.py
+++ development/nautobot_config.py
@@ -175,7 +175,7 @@ PLUGINS_CONFIG = {
         "postprocessing_callables": os.environ.get("POSTPROCESSING_CALLABLES", []),
         "postprocessing_subscribed": os.environ.get("POSTPROCESSING_SUBSCRIBED", []),
         "jinja_env_trim_blocks": is_truthy(os.getenv("NAUTOBOT_JINJA_ENV_TRIM_BLOCKS", True)),
-        "jinja_env_lstrip_blocks": is_truthy(os.getenv("NAUTOBOT_JINJA_ENV_TRIM_BLOCKS", True)),
+        "jinja_env_lstrip_blocks": is_truthy(os.getenv("NAUTOBOT_JINJA_ENV_TRIM_BLOCKS", False)),
         # The platform_slug_map maps an arbitrary platform slug to its corresponding parser.
         # Use this if the platform slug names in your Nautobot instance don't correspond exactly
         # to the Nornir driver names ("arista_eos", "cisco_ios", etc.).

01f3a114fb2f2f87083e5206cf1b1d9bd8756562
Jeff Kala <jeff.kala@networktocode.com>
2023-09-07 01:29:36 +0000

    Jeff Contribution to Remediation & Plan

--- nautobot_golden_config/forms.py
+++ nautobot_golden_config/forms.py
@@ -1,18 +1,18 @@
 """Forms for Device Configuration Backup."""
 # pylint: disable=too-many-ancestors
 
-from django import forms
+import json
 
 import nautobot.extras.forms as extras_forms
 import nautobot.utilities.forms as utilities_forms
-from nautobot.dcim.models import Device, Platform, Region, Site, DeviceRole, DeviceType, Manufacturer, Rack, RackGroup
-from nautobot.extras.models import Status, GitRepository, DynamicGroup
+from django import forms
+from nautobot.dcim.models import Device, DeviceRole, DeviceType, Manufacturer, Platform, Rack, RackGroup, Region, Site
+from nautobot.extras.forms import NautobotBulkEditForm, NautobotFilterForm, NautobotModelForm
+from nautobot.extras.models import DynamicGroup, GitRepository, JobResult, Status, Tag
 from nautobot.tenancy.models import Tenant, TenantGroup
-from nautobot.utilities.forms import SlugField
-from nautobot.extras.forms import NautobotFilterForm, NautobotBulkEditForm, NautobotModelForm
-
-
+from nautobot.utilities.forms import add_blank_choice, DatePicker, SlugField, TagFilterField
 from nautobot_golden_config import models
+from nautobot_golden_config.choices import ComplianceRuleConfigTypeChoice, ConfigPlanTypeChoice, RemediationTypeChoice
 
 # ConfigCompliance
 
@@ -123,6 +123,7 @@ class ComplianceRuleForm(NautobotModelForm):
             "config_type",
             "match_config",
             "custom_compliance",
+            "config_remediation",
         )
 
 
@@ -145,6 +146,14 @@ class ComplianceRuleBulkEditForm(NautobotBulkEditForm):
     """BulkEdit form for ComplianceRule instances."""
 
     pk = forms.ModelMultipleChoiceField(queryset=models.ComplianceRule.objects.all(), widget=forms.MultipleHiddenInput)
+    description = forms.CharField(max_length=200, required=False)
+    config_type = forms.ChoiceField(
+        required=False,
+        choices=utilities_forms.add_blank_choice(ComplianceRuleConfigTypeChoice),
+    )
+    config_ordered = forms.NullBooleanField(required=False, widget=utilities_forms.BulkEditNullBooleanSelect())
+    custom_compliance = forms.NullBooleanField(required=False, widget=utilities_forms.BulkEditNullBooleanSelect())
+    config_remediation = forms.NullBooleanField(required=False, widget=utilities_forms.BulkEditNullBooleanSelect())
 
     class Meta:
         """Boilerplate form Meta data for ComplianceRule."""
@@ -191,6 +200,7 @@ class ComplianceFeatureBulkEditForm(NautobotBulkEditForm):
     pk = forms.ModelMultipleChoiceField(
         queryset=models.ComplianceFeature.objects.all(), widget=forms.MultipleHiddenInput
     )
+    description = forms.CharField(max_length=200, required=False)
 
     class Meta:
         """Boilerplate form Meta data for ComplianceFeature."""
@@ -244,6 +254,7 @@ class ConfigRemoveBulkEditForm(NautobotBulkEditForm):
     """BulkEdit form for ConfigRemove instances."""
 
     pk = forms.ModelMultipleChoiceField(queryset=models.ConfigRemove.objects.all(), widget=forms.MultipleHiddenInput)
+    description = forms.CharField(max_length=200, required=False)
 
     class Meta:
         """Boilerplate form Meta data for ConfigRemove."""
@@ -309,6 +320,7 @@ class ConfigReplaceBulkEditForm(NautobotBulkEditForm):
     """BulkEdit form for ConfigReplace instances."""
 
     pk = forms.ModelMultipleChoiceField(queryset=models.ConfigReplace.objects.all(), widget=forms.MultipleHiddenInput)
+    description = forms.CharField(max_length=200, required=False)
 
     class Meta:
         """Boilerplate form Meta data for ConfigReplace."""
@@ -390,3 +402,233 @@ class GoldenConfigSettingBulkEditForm(NautobotBulkEditForm):
         """Boilerplate form Meta data for GoldenConfigSetting."""
 
         nullable_fields = []
+
+
+# Remediation Setting
+class RemediationSettingForm(NautobotModelForm):
+    """Create/Update Form for Remediation Settings instances."""
+
+    class Meta:
+        """Boilerplate form Meta data for Remediation Settings."""
+
+        model = models.RemediationSetting
+        fields = (
+            "platform",
+            "remediation_type",
+            "remediation_options",
+        )
+
+
+class RemediationSettingFilterForm(NautobotFilterForm):
+    """Filter Form for Remediation Settings."""
+
+    model = models.RemediationSetting
+    q = forms.CharField(required=False, label="Search")
+    platform = utilities_forms.DynamicModelMultipleChoiceField(
+        queryset=Platform.objects.all(), required=False, display_field="name", to_field_name="name"
+    )
+    remediation_type = forms.ChoiceField(
+        choices=add_blank_choice(RemediationTypeChoice), required=False, label="Remediation Type"
+    )
+
+
+class RemediationSettingCSVForm(extras_forms.CustomFieldModelCSVForm):
+    """CSV Form for RemediationSetting instances."""
+
+    class Meta:
+        """Boilerplate form Meta data for RemediationSetting."""
+
+        model = models.RemediationSetting
+        fields = models.RemediationSetting.csv_headers
+
+
+class RemediationSettingBulkEditForm(NautobotBulkEditForm):
+    """BulkEdit form for RemediationSetting instances."""
+
+    pk = forms.ModelMultipleChoiceField(
+        queryset=models.RemediationSetting.objects.all(), widget=forms.MultipleHiddenInput
+    )
+    remediation_type = forms.ChoiceField(choices=RemediationTypeChoice, label="Remediation Type")
+
+    class Meta:
+        """Boilerplate form Meta data for RemediationSetting."""
+
+        nullable_fields = []
+
+
+# ConfigPlan
+
+
+class ConfigPlanForm(NautobotModelForm):
+    """Form for ConfigPlan instances."""
+
+    plan_type = forms.ChoiceField(choices=add_blank_choice(ConfigPlanTypeChoice), required=True, label="Plan Type")
+    change_control_id = forms.CharField(required=False, label="Change Control ID")
+    change_control_url = forms.URLField(required=False, label="Change Control URL")
+
+    feature = utilities_forms.DynamicModelMultipleChoiceField(
+        queryset=models.ComplianceFeature.objects.all(),
+        display_field="name",
+        required=False,
+        help_text="Note: Selecting no features will generate plans for all applicable features.",
+    )
+    commands = forms.CharField(
+        widget=forms.Textarea,
+        help_text=(
+            "Enter your configuration template here representing CLI configuration.<br>"
+            'You may use Jinja2 templating. Example: <code>{% if "foo" in bar %}foo{% endif %}</code><br>'
+            "You can also reference the device object with <code>obj</code>.<br>"
+            "For example: <code>hostname {{ obj.name }}</code> or <code>ip address {{ obj.primary_ip4.host }}</code>"
+        ),
+        required=True,
+    )
+
+    tenant_group = utilities_forms.DynamicModelMultipleChoiceField(queryset=TenantGroup.objects.all(), required=False)
+    tenant = utilities_forms.DynamicModelMultipleChoiceField(queryset=Tenant.objects.all(), required=False)
+    # Requires https://github.com/nautobot/nautobot-plugin-golden-config/issues/430
+    # location = utilities_forms.DynamicModelMultipleChoiceField(queryset=Location.objects.all(), required=False)
+    region = utilities_forms.DynamicModelMultipleChoiceField(queryset=Region.objects.all(), required=False)
+    site = utilities_forms.DynamicModelMultipleChoiceField(queryset=Site.objects.all(), required=False)
+    rack_group = utilities_forms.DynamicModelMultipleChoiceField(queryset=RackGroup.objects.all(), required=False)
+    rack = utilities_forms.DynamicModelMultipleChoiceField(queryset=Rack.objects.all(), required=False)
+    role = utilities_forms.DynamicModelMultipleChoiceField(queryset=DeviceRole.objects.all(), required=False)
+    manufacturer = utilities_forms.DynamicModelMultipleChoiceField(queryset=Manufacturer.objects.all(), required=False)
+    platform = utilities_forms.DynamicModelMultipleChoiceField(queryset=Platform.objects.all(), required=False)
+    device_type = utilities_forms.DynamicModelMultipleChoiceField(queryset=DeviceType.objects.all(), required=False)
+    device = utilities_forms.DynamicModelMultipleChoiceField(queryset=Device.objects.all(), required=False)
+    tag = utilities_forms.DynamicModelMultipleChoiceField(
+        queryset=Tag.objects.all(), query_params={"content_types": "dcim.device"}, required=False
+    )
+    status = utilities_forms.DynamicModelMultipleChoiceField(
+        queryset=Status.objects.all(), query_params={"content_types": "dcim.device"}, required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        """Method to get data from Python -> Django template -> JS  in support of toggle form fields."""
+        super().__init__(*args, **kwargs)
+        hide_form_data = [
+            {
+                "event_field": "id_plan_type",
+                "values": [
+                    {"name": "manual", "show": ["id_commands"], "hide": ["id_feature"]},
+                    {"name": "missing", "show": ["id_feature"], "hide": ["id_commands"]},
+                    {"name": "intended", "show": ["id_feature"], "hide": ["id_commands"]},
+                    {"name": "remediation", "show": ["id_feature"], "hide": ["id_commands"]},
+                    {"name": "", "show": [], "hide": ["id_commands", "id_feature"]},
+                ],
+            }
+        ]
+        # Example of how to use this `JSON.parse('{{ form.hide_form_data|safe }}')`
+        self.hide_form_data = json.dumps(hide_form_data)
+
+    class Meta:
+        """Boilerplate form Meta data for ConfigPlan."""
+
+        model = models.ConfigPlan
+        fields = (
+            "plan_type",
+            "change_control_id",
+            "feature",
+            "commands",
+            "tenant",
+            # "location", Requires https://github.com/nautobot/nautobot-plugin-golden-config/issues/430
+            "region",
+            "site",
+            "rack_group",
+            "rack",
+            "role",
+            "manufacturer",
+            "platform",
+            "device_type",
+            "device",
+            "tag",
+            "status",
+        )
+
+
+class ConfigPlanUpdateForm(NautobotModelForm):
+    """Form for ConfigPlan instances."""
+
+    change_control_id = forms.CharField(required=False, label="Change Control ID")
+    change_control_url = forms.URLField(required=False, label="Change Control URL")
+    status = utilities_forms.DynamicModelChoiceField(
+        queryset=Status.objects.all(),
+        query_params={"content_types": models.ConfigPlan._meta.label_lower},
+        required=False,
+    )
+    tag = utilities_forms.DynamicModelMultipleChoiceField(
+        queryset=Tag.objects.all(), query_params={"content_types": "dcim.device"}, required=False
+    )
+
+    class Meta:
+        """Boilerplate form Meta data for ConfigPlan."""
+
+        model = models.ConfigPlan
+        fields = (
+            "change_control_id",
+            "change_control_url",
+            "tag",
+            "status",
+        )
+
+
+class ConfigPlanFilterForm(NautobotFilterForm):
+    """Filter Form for ConfigPlan."""
+
+    model = models.ConfigPlan
+
+    q = forms.CharField(required=False, label="Search")
+    device_id = utilities_forms.DynamicModelMultipleChoiceField(
+        queryset=Device.objects.all(), required=False, null_option="None", label="Device"
+    )
+    created__lte = forms.DateTimeField(label="Created Before", required=False, widget=DatePicker())
+    created__gte = forms.DateTimeField(label="Created After", required=False, widget=DatePicker())
+    plan_type = forms.ChoiceField(
+        choices=add_blank_choice(ConfigPlanTypeChoice), required=False, widget=forms.Select(), label="Plan Type"
+    )
+    feature = utilities_forms.DynamicModelMultipleChoiceField(
+        queryset=models.ComplianceFeature.objects.all(),
+        required=False,
+        null_option="None",
+        label="Feature",
+        to_field_name="name",
+    )
+    change_control_id = forms.CharField(required=False, label="Change Control ID")
+    job_result_id = utilities_forms.DynamicModelMultipleChoiceField(
+        queryset=JobResult.objects.all(),
+        query_params={"nautobot_golden_config_config_plan_null": True},
+        label="Job Result",
+        required=False,
+        display_field="id",
+    )
+    status = utilities_forms.DynamicModelMultipleChoiceField(
+        required=False,
+        queryset=Status.objects.all(),
+        query_params={"content_types": models.ConfigPlan._meta.label_lower},
+        display_field="label",
+        label="Status",
+        to_field_name="name",
+    )
+    tag = TagFilterField(model)
+
+
+class ConfigPlanBulkEditForm(extras_forms.TagsBulkEditFormMixin, NautobotBulkEditForm):
+    """BulkEdit form for ConfigPlan instances."""
+
+    pk = forms.ModelMultipleChoiceField(queryset=models.ConfigPlan.objects.all(), widget=forms.MultipleHiddenInput)
+    status = utilities_forms.DynamicModelChoiceField(
+        queryset=Status.objects.all(),
+        query_params={"content_types": models.ConfigPlan._meta.label_lower},
+        required=False,
+    )
+    change_control_id = forms.CharField(required=False, label="Change Control ID")
+    change_control_url = forms.URLField(required=False, label="Change Control URL")
+
+    class Meta:
+        """Boilerplate form Meta data for ConfigPlan."""
+
+        nullable_fields = [
+            "change_control_id",
+            "change_control_url",
+            "tags",
+        ]
--- nautobot_golden_config/jobs.py
+++ nautobot_golden_config/jobs.py
@@ -5,13 +5,36 @@ from datetime import datetime
 
 from nautobot.dcim.models import Device, DeviceRole, DeviceType, Manufacturer, Platform, Rack, RackGroup, Region, Site
 from nautobot.extras.datasources.git import ensure_git_repository
-from nautobot.extras.jobs import BooleanVar, Job, MultiObjectVar, ObjectVar
+from nautobot.extras.jobs import (
+    BooleanVar,
+    ChoiceVar,
+    Job,
+    JobButtonReceiver,
+    MultiObjectVar,
+    ObjectVar,
+    StringVar,
+    TextVar,
+)
 from nautobot.extras.models import DynamicGroup, GitRepository, Status, Tag
 from nautobot.tenancy.models import Tenant, TenantGroup
+from nornir_nautobot.exceptions import NornirNautobotException
+
+from nautobot_golden_config.choices import ConfigPlanTypeChoice
+from nautobot_golden_config.models import ComplianceFeature, ConfigPlan
 from nautobot_golden_config.nornir_plays.config_backup import config_backup
 from nautobot_golden_config.nornir_plays.config_compliance import config_compliance
+from nautobot_golden_config.nornir_plays.config_deployment import config_deployment
 from nautobot_golden_config.nornir_plays.config_intended import config_intended
-from nautobot_golden_config.utilities.constant import ENABLE_BACKUP, ENABLE_COMPLIANCE, ENABLE_INTENDED
+from nautobot_golden_config.utilities.config_plan import (
+    config_plan_default_status,
+    generate_config_set_from_compliance_feature,
+    generate_config_set_from_manual,
+)
+from nautobot_golden_config.utilities.constant import (
+    ENABLE_BACKUP,
+    ENABLE_COMPLIANCE,
+    ENABLE_INTENDED,
+)
 from nautobot_golden_config.utilities.git import GitRepo
 from nautobot_golden_config.utilities.helper import get_job_filter
 
@@ -268,6 +291,177 @@ class AllDevicesGoldenConfig(Job):
             ComplianceJob().run.__func__(self, data, True)  # pylint: disable=too-many-function-args
 
 
+class GenerateConfigPlans(Job, FormEntry):
+    """Job to generate config plans."""
+
+    # Device QS Filters
+    tenant_group = FormEntry.tenant_group
+    tenant = FormEntry.tenant
+    region = FormEntry.region
+    site = FormEntry.site
+    rack_group = FormEntry.rack_group
+    rack = FormEntry.rack
+    role = FormEntry.role
+    manufacturer = FormEntry.manufacturer
+    platform = FormEntry.platform
+    device_type = FormEntry.device_type
+    device = FormEntry.device
+    tag = FormEntry.tag
+    status = FormEntry.status
+    debug = FormEntry.debug
+
+    # Config Plan generation fields
+    plan_type = ChoiceVar(choices=ConfigPlanTypeChoice.CHOICES)
+    feature = MultiObjectVar(model=ComplianceFeature, required=False)
+    change_control_id = StringVar(required=False)
+    change_control_url = StringVar(required=False)
+    commands = TextVar(required=False)
+
+    class Meta:
+        """Meta object boilerplate for config plan generation."""
+
+        name = "Generate Config Plans"
+        description = "Generate config plans for devices."
+        # Defaulting to hidden as this should be primarily called by the View
+        hidden = True
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the job."""
+        super().__init__(*args, **kwargs)
+        self._plan_type = None
+        self._feature = None
+        self._change_control_id = None
+        self._change_control_url = None
+        self._commands = None
+        self._device_qs = Device.objects.none()
+        self._status = config_plan_default_status()
+
+    def _validate_inputs(self, data):
+        self._plan_type = data["plan_type"]
+        self._feature = data.get("feature", [])
+        self._change_control_id = data.get("change_control_id", "")
+        self._change_control_url = data.get("change_control_url", "")
+        self._commands = data.get("commands", "")
+        if self._plan_type in ["intended", "missing", "remediation"]:
+            if not self._feature:
+                self._feature = ComplianceFeature.objects.all()
+        if self._plan_type in ["manual"]:
+            if not self._commands:
+                self.log_failure("No commands entered for config plan generation.")
+                return False
+        return True
+
+    def _generate_config_plan_from_feature(self):
+        """Generate config plans from features."""
+        for device in self._device_qs:
+            config_sets = []
+            features = []
+            for feature in self._feature:
+                config_set = generate_config_set_from_compliance_feature(device, self._plan_type, feature)
+                if not config_set:
+                    continue
+                config_sets.append(config_set)
+                features.append(feature)
+
+            if not config_sets:
+                _features = ", ".join([str(feat) for feat in self._feature])
+                self.log_debug(f"Device `{device}` does not have `{self._plan_type}` configs for `{_features}`.")
+                continue
+            config_plan = ConfigPlan.objects.create(
+                device=device,
+                plan_type=self._plan_type,
+                config_set="\n".join(config_sets),
+                change_control_id=self._change_control_id,
+                change_control_url=self._change_control_url,
+                status=self._status,
+                job_result=self.job_result,
+            )
+            config_plan.feature.set(features)
+            config_plan.validated_save()
+            _features = ", ".join([str(feat) for feat in features])
+            self.log_success(obj=config_plan, message=f"Config plan created for `{device}` with feature `{_features}`.")
+
+    def _generate_config_plan_from_manual(self):
+        """Generate config plans from manual."""
+        default_context = {
+            "request": self.request,
+            "user": self.request.user,
+        }
+        for device in self._device_qs:
+            config_set = generate_config_set_from_manual(device, self._commands, context=default_context)
+            if not config_set:
+                self.log_debug(f"Device {self.device} did not return a rendered config set from the provided commands.")
+                continue
+            config_plan = ConfigPlan.objects.create(
+                device=device,
+                plan_type=self._plan_type,
+                config_set=config_set,
+                change_control_id=self._change_control_id,
+                change_control_url=self._change_control_url,
+                status=self._status,
+                job_result=self.job_result,
+            )
+            self.log_success(obj=config_plan, message=f"Config plan created for {device} with manual commands.")
+
+    def run(self, data, commit):
+        """Run config plan generation process."""
+        self.log_debug("Starting config plan generation job.")
+        if not self._validate_inputs(data):
+            return
+        try:
+            self._device_qs = get_job_filter(data)
+        except NornirNautobotException as exc:
+            self.log_failure(str(exc))
+            return
+        if self._plan_type in ["intended", "missing", "remediation"]:
+            self.log_debug("Starting config plan generation for compliance features.")
+            self._generate_config_plan_from_feature()
+        elif self._plan_type in ["manual"]:
+            self.log_debug("Starting config plan generation for manual commands.")
+            self._generate_config_plan_from_manual()
+        else:
+            self.log_failure(f"Unknown config plan type {self._plan_type}.")
+            return
+
+
+class DeployConfigPlans(Job):
+    """Job to deploy config plans."""
+
+    config_plan = MultiObjectVar(model=ConfigPlan, required=True)
+    debug = BooleanVar(description="Enable for more verbose debug logging")
+
+    class Meta:
+        """Meta object boilerplate for config plan deployment."""
+
+        name = "Deploy Config Plans"
+        description = "Deploy config plans to devices."
+
+    def run(self, data, commit):
+        """Run config plan deployment process."""
+        self.log_debug("Starting config plan deployment job.")
+        config_deployment(self, data, commit)
+        if commit and not self.failed:
+            config_plan_qs = data["config_plan"]
+            config_plan_qs.delete()
+
+
+class DeployConfigPlanJobButtonReceiver(JobButtonReceiver):
+    """Job button to deploy a config plan."""
+
+    class Meta:
+        """Meta object boilerplate for config plan deployment job button."""
+
+        name = "Deploy Config Plan (Job Button Receiver)"
+
+    def receive_job_button(self, obj):
+        """Run config plan deployment process."""
+        self.log_debug("Starting config plan deployment job.")
+        data = {"debug": False, "config_plan": ConfigPlan.objects.filter(id=obj.id)}
+        config_deployment(self, data, commit=True)
+        if not self.failed:
+            obj.delete()
+
+
 # Conditionally allow jobs based on whether or not turned on.
 jobs = []
 if ENABLE_BACKUP:
@@ -276,4 +470,12 @@ if ENABLE_INTENDED:
     jobs.append(IntendedJob)
 if ENABLE_COMPLIANCE:
     jobs.append(ComplianceJob)
-jobs.extend([AllGoldenConfig, AllDevicesGoldenConfig])
+jobs.extend(
+    [
+        AllGoldenConfig,
+        AllDevicesGoldenConfig,
+        GenerateConfigPlans,
+        DeployConfigPlans,
+        DeployConfigPlanJobButtonReceiver,
+    ]
+)

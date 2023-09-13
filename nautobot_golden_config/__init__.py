3f231f2b053c08f98dfa21be48f38f3be446b926
bakebot <info@networktocode.com>
2023-09-13 09:54:35 +0000

    Cookie initialy baked by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "develop",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-plugin-golden-config.git",
        "path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-golden-config",
        "repository_path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-golden-config",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@itdependsnetworks @jeffkala @nkallergis",
            "full_name": "Network to Code, LLC",
            "email": "opensource@networktocode.com",
            "github_org": "nautobot",
            "plugin_name": "nautobot_golden_config",
            "verbose_name": "Golden Config",
            "plugin_slug": "nautobot-golden-config",
            "project_slug": "nautobot-plugin-golden-config",
            "repo_url": "https://github.com/nautobot/nautobot-plugin-golden-config",
            "base_url": "golden-config",
            "min_nautobot_version": "1.4.0",
            "max_nautobot_version": "1.9999",
            "nautobot_version": "latest",
            "camel_name": "NautobotGoldenConfig",
            "project_short_description": "A plugin for configuration on nautobot",
            "version": "1.0.0",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/golden-config/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/opt/ntc/drift-manager/outputs",
            "_repo_dir": "/opt/ntc/drift-manager/outputs/.cookiecutters/cookiecutter-nautobot-app/nautobot-app"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": []
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "./setup-cookie-golden-config.json",
        "output_dir": "./outputs",
        "push": true,
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "template_dir": "nautobot-app",
        "template_ref": "develop",
        "pull_request": null,
        "post_action": [],
        "disable_post_actions": false
    }
    ```

--- nautobot_golden_config/__init__.py
+++ nautobot_golden_config/__init__.py
@@ -2,62 +2,25 @@
 # Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
 from importlib import metadata
 
-
 __version__ = metadata.version(__name__)
 
-from jinja2 import StrictUndefined
-from django.db.models.signals import post_migrate
-from nautobot.core.signals import nautobot_database_ready
-from nautobot.extras.plugins import PluginConfig
+from nautobot.extras.plugins import NautobotAppConfig
 
 
-class GoldenConfig(PluginConfig):
+class NautobotGoldenConfigConfig(NautobotAppConfig):
     """Plugin configuration for the nautobot_golden_config plugin."""
 
     name = "nautobot_golden_config"
-    verbose_name = "Golden Configuration"
+    verbose_name = "Golden Config"
     version = __version__
     author = "Network to Code, LLC"
-    author_email = "opensource@networktocode.com"
-    description = "Nautobot Apps that embraces NetDevOps and automates configuration backups, performs configuration compliance, and generates intended configurations. Includes native Git integration and gives users the flexibility to mix and match the supported features."
+    description = "A plugin for configuration on nautobot."
     base_url = "golden-config"
-    min_version = "1.6.1"
-    max_version = "1.99"
-    default_settings = {
-        "enable_backup": True,
-        "enable_compliance": True,
-        "enable_intended": True,
-        "enable_sotagg": True,
-        "enable_postprocessing": False,
-        "postprocessing_callables": [],
-        "postprocessing_subscribed": [],
-        "per_feature_bar_width": 0.3,
-        "per_feature_width": 13,
-        "per_feature_height": 4,
-        "get_custom_compliance": None,
-        "jinja_env": {
-            "undefined": StrictUndefined,
-            "trim_blocks": True,
-            "lstrip_blocks": False,
-        },
-    }
-
-    def ready(self):
-        """Register custom signals."""
-        from nautobot_golden_config.models import ConfigCompliance  # pylint: disable=import-outside-toplevel
-
-        # pylint: disable=import-outside-toplevel
-        from .signals import (
-            config_compliance_platform_cleanup,
-            post_migrate_create_statuses,
-            post_migrate_create_job_button,
-        )
-
-        nautobot_database_ready.connect(post_migrate_create_statuses, sender=self)
-        nautobot_database_ready.connect(post_migrate_create_job_button, sender=self)
-
-        super().ready()
-        post_migrate.connect(config_compliance_platform_cleanup, sender=ConfigCompliance)
+    required_settings = []
+    min_version = "1.4.0"
+    max_version = "1.9999"
+    default_settings = {}
+    caching_config = {}
 
 
-config = GoldenConfig  # pylint:disable=invalid-name
+config = NautobotGoldenConfigConfig  # pylint:disable=invalid-name

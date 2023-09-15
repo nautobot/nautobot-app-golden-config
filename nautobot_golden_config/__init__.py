36d66a9b3337a7db5d654551515ceb67ae796338
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2023-09-15 14:53:25 -0600

    prepping for 1.6.0 release (#576)
    * prepping for 1.6.0 release
    * Update platform mapping (#586)
    
    ---------
    
    Co-authored-by: matt852 <matt.vitale@networktocode.com>

--- nautobot_golden_config/__init__.py
+++ nautobot_golden_config/__init__.py
@@ -19,7 +19,7 @@ class GoldenConfig(PluginConfig):
     version = __version__
     author = "Network to Code, LLC"
     author_email = "opensource@networktocode.com"
-    description = "Nautobot Apps that embraces NetDevOps and automates configuration backups, performs configuration compliance, and generates intended configurations. Includes native Git integration and gives users the flexibility to mix and match the supported features."
+    description = "Nautobot Apps that embraces NetDevOps and automates configuration backups, performs configuration compliance, generates intended configurations, and has config remediation and deployment features. Includes native Git integration and gives users the flexibility to mix and match the supported features."
     base_url = "golden-config"
     min_version = "1.6.1"
     max_version = "1.99"

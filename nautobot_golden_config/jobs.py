403eaf22340f853063450e61b1e7c4624f3cccbe
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-09-19 14:16:18 -0500

    fix: 🐛 Remove FormEntry mixin from AllGoldenConfig

--- nautobot_golden_config/jobs.py
+++ nautobot_golden_config/jobs.py
@@ -187,7 +187,7 @@ class BackupJob(Job, FormEntry):
             backup_repo.push()
 
 
-class AllGoldenConfig(Job, FormEntry):
+class AllGoldenConfig(Job):
     """Job to to run all three jobs against a single device."""
 
     device = ObjectVar(model=Device, required=True)

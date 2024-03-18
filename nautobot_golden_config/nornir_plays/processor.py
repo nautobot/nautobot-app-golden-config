a1afde81538f7ab0bef5ae6bb9017ca2053e33b5
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2024-03-18 13:21:59 -0500

    refactor: ♻️ Disable task_completed and _find_result_exceptions to match 1.6.4.

--- nautobot_golden_config/nornir_plays/processor.py
+++ nautobot_golden_config/nornir_plays/processor.py
@@ -12,31 +12,31 @@ class ProcessGoldenConfig(BaseLoggingProcessor):
         """Set logging facility."""
         self.logger = logger
 
-    def task_completed(self, task: Task, result: AggregatedResult) -> None:
-        """Task outside of thread to determine what to do."""
-        if result.failed:
-            self.logger.error(f"{task.name} failed: {result}")
-            raise ValueError(result)
+    # def task_completed(self, task: Task, result: AggregatedResult) -> None:
+    #     """Task outside of thread to determine what to do."""
+    #     if result.failed:
+    #         self.logger.error(f"{task.name} failed: {result}")
+    #         raise ValueError(result)
 
-    def _find_result_exceptions(self, result):
-        """Walk the results and return only valid Exceptions.
+    # def _find_result_exceptions(self, result):
+    #     """Walk the results and return only valid Exceptions.
 
-        NornirNautobotException is expected to be raised in some situations.
-        """
-        valid_exceptions = []
-        if result.failed:
-            if isinstance(result, MultiResult) and hasattr(result, "exception"):
-                if not isinstance(result.exception, NornirNautobotException):
-                    # return exception and traceback output
-                    valid_exceptions.append([result.exception, result.result])
-            elif isinstance(result, Result) and hasattr(result, "exception"):
-                if not isinstance(result.exception, NornirNautobotException):
-                    # return exception and traceback output
-                    valid_exceptions.append([result.exception, result.result])
-            elif hasattr(result, "exception") and hasattr(result.exception, "result"):
-                for exception_result in result.exception.result:
-                    valid_exceptions += self._find_result_exceptions(exception_result)
-        return valid_exceptions
+    #     NornirNautobotException is expected to be raised in some situations.
+    #     """
+    #     valid_exceptions = []
+    #     if result.failed:
+    #         if isinstance(result, MultiResult) and hasattr(result, "exception"):
+    #             if not isinstance(result.exception, NornirNautobotException):
+    #                 # return exception and traceback output
+    #                 valid_exceptions.append([result.exception, result.result])
+    #         elif isinstance(result, Result) and hasattr(result, "exception"):
+    #             if not isinstance(result.exception, NornirNautobotException):
+    #                 # return exception and traceback output
+    #                 valid_exceptions.append([result.exception, result.result])
+    #         elif hasattr(result, "exception") and hasattr(result.exception, "result"):
+    #             for exception_result in result.exception.result:
+    #                 valid_exceptions += self._find_result_exceptions(exception_result)
+    #     return valid_exceptions
 
     def task_instance_completed(self, task: Task, host: Host, result: MultiResult) -> None:
         """Nornir processor task completion for golden configurations.

9fdf781a700b11d1803305e9fd7d1e5d5c5b57d9
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2024-03-21 16:38:11 -0500

    revert: ⏪️ Return task_instance_completed and helper function back in processor.
    Still leaving Exceptions removed as they cause any other Jobs in AllGoldenConfig Jobs to never run.

--- nautobot_golden_config/nornir_plays/processor.py
+++ nautobot_golden_config/nornir_plays/processor.py
@@ -12,31 +12,25 @@ class ProcessGoldenConfig(BaseLoggingProcessor):
         """Set logging facility."""
         self.logger = logger
 
-    # def task_completed(self, task: Task, result: AggregatedResult) -> None:
-    #     """Task outside of thread to determine what to do."""
-    #     if result.failed:
-    #         self.logger.error(f"{task.name} failed: {result}")
-    #         raise ValueError(result)
+    def _find_result_exceptions(self, result):
+        """Walk the results and return only valid Exceptions.
 
-    # def _find_result_exceptions(self, result):
-    #     """Walk the results and return only valid Exceptions.
-
-    #     NornirNautobotException is expected to be raised in some situations.
-    #     """
-    #     valid_exceptions = []
-    #     if result.failed:
-    #         if isinstance(result, MultiResult) and hasattr(result, "exception"):
-    #             if not isinstance(result.exception, NornirNautobotException):
-    #                 # return exception and traceback output
-    #                 valid_exceptions.append([result.exception, result.result])
-    #         elif isinstance(result, Result) and hasattr(result, "exception"):
-    #             if not isinstance(result.exception, NornirNautobotException):
-    #                 # return exception and traceback output
-    #                 valid_exceptions.append([result.exception, result.result])
-    #         elif hasattr(result, "exception") and hasattr(result.exception, "result"):
-    #             for exception_result in result.exception.result:
-    #                 valid_exceptions += self._find_result_exceptions(exception_result)
-    #     return valid_exceptions
+        NornirNautobotException is expected to be raised in some situations.
+        """
+        valid_exceptions = []
+        if result.failed:
+            if isinstance(result, MultiResult) and hasattr(result, "exception"):
+                if not isinstance(result.exception, NornirNautobotException):
+                    # return exception and traceback output
+                    valid_exceptions.append([result.exception, result.result])
+            elif isinstance(result, Result) and hasattr(result, "exception"):
+                if not isinstance(result.exception, NornirNautobotException):
+                    # return exception and traceback output
+                    valid_exceptions.append([result.exception, result.result])
+            elif hasattr(result, "exception") and hasattr(result.exception, "result"):
+                for exception_result in result.exception.result:
+                    valid_exceptions += self._find_result_exceptions(exception_result)
+        return valid_exceptions
 
     def task_instance_completed(self, task: Task, host: Host, result: MultiResult) -> None:
         """Nornir processor task completion for golden configurations.
@@ -50,13 +44,9 @@ class ProcessGoldenConfig(BaseLoggingProcessor):
             None
         """
         host.close_connections()
+        exceptions = self._find_result_exceptions(result)
 
-        # Complex logic to see if the task exception is expected, which is depicted by
-        # a sub task raising a NornirNautobotException.
-        if result.failed:
-            for level_1_result in result:
-                if hasattr(level_1_result, "exception") and hasattr(level_1_result.exception, "result"):
-                    for level_2_result in level_1_result.exception.result:
-                        if isinstance(level_2_result.exception, NornirNautobotException):
-                            return
-            self.logger.error(f"{task.name} failed: {result.exception}")
+        if result.failed and exceptions:
+            exception_string = ", ".join([str(e[0]) for e in exceptions])
+            # Log only exception summary to users
+            self.logger.error(f"{task.name} failed: {exception_string}", extra={"object": task.host.data["obj"]})

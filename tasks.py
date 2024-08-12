1e6e9e5ef3b1ac34894247ad50b45ba50b3e54f1
Leo Kirchner <leo.kirchner@networktocode.com>
2024-08-12 15:14:55 +0200

    switches to docker compose over docker-compose

--- tasks.py
+++ tasks.py
@@ -92,7 +92,7 @@ def docker_compose(context, command, **kwargs):
         "PYTHON_VER": context.nautobot_golden_config.python_ver,
     }
     compose_command_tokens = [
-        "docker-compose",
+        "docker compose",
         f"--project-name {context.nautobot_golden_config.project_name}",
         f'--project-directory "{context.nautobot_golden_config.compose_dir}"',
     ]

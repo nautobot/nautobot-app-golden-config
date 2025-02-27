74560f9a968502bac4dba47c23ce421635dab228
James Williams <amascuba@gmail.com>
2025-02-27 09:41:41 -0600

    remove checking settings from nornir itself

--- nautobot_golden_config/nornir_plays/config_deployment.py
+++ nautobot_golden_config/nornir_plays/config_deployment.py
@@ -28,7 +28,7 @@ InventoryPluginRegister.register("nautobot-inventory", NautobotORMInventory)
 
 @close_threaded_db_connections
 def run_deployment(  # pylint: disable=too-many-arguments, too-many-locals
-    task: Task, logger: logging.Logger, device_to_settings_map, config_plan_qs, deploy_job_result, job_request
+    task: Task, logger: logging.Logger, config_plan_qs, deploy_job_result, job_request
 ) -> Result:
     """Deploy configurations to device."""
     obj = task.host.data["obj"]

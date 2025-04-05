76e47e6769800125b4be6ac010f7e8bf39a6bf6b
Jeff Kala <jeff.l.kala@gmail.com>
2025-04-05 13:53:46 -0500

    remove task.py nb version change

--- tasks.py
+++ tasks.py
@@ -51,7 +51,7 @@ namespace = Collection("nautobot_golden_config")
 namespace.configure(
     {
         "nautobot_golden_config": {
-            "nautobot_ver": "2.4.5",
+            "nautobot_ver": "2.4.2",
             "project_name": "nautobot-golden-config",
             "python_ver": "3.11",
             "local": False,

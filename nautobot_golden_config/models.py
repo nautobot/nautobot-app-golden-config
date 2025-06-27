2a52816effb59b6b8d57b213b7ac21b474b9ccec
jtdub <amascuba@gmail.com>
2025-06-26 20:24:10 -0500

    pylint

--- nautobot_golden_config/models.py
+++ nautobot_golden_config/models.py
@@ -233,7 +233,7 @@ def _get_hierconfig_remediation(obj):
             f"Cannot instantiate HierConfig on {obj.device.name}, check Device, Platform and Hier Options."
         ) from err
 
-    hierconfig_wfr.remediation_config()
+    hierconfig_wfr.remediation_config
     remediation_config = hierconfig_wfr.remediation_config_filtered_text(include_tags={}, exclude_tags={})
 
     return remediation_config
@@ -252,7 +252,7 @@ for custom_function, custom_type in CUSTOM_FUNCTIONS.items():
         try:
             FUNC_MAPPER[custom_type] = import_string(PLUGIN_CFG[custom_function])
         except Exception as error:  # pylint: disable=broad-except
-            msg = (
+            msg = (  # pylint: disable=invalid-name
                 "There was an issue attempting to import the custom function of"
                 f"{PLUGIN_CFG[custom_function]}, this is expected with a local configuration issue "
                 "and not related to the Golden Configuration App, please contact your system admin for further details"

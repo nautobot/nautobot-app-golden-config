f1666135690984dd61111bbd869fce1e896e9d45
julmanglano <julian.manglano@networktocode.com>
2025-12-09 17:18:46 +0100

    render in helper

--- nautobot_golden_config/utilities/config_plan.py
+++ nautobot_golden_config/utilities/config_plan.py
@@ -6,7 +6,7 @@ from nautobot.extras.models import Status
 
 from nautobot_golden_config.models import ComplianceFeature
 from nautobot_golden_config.utilities.constant import DEFAULT_DEPLOY_STATUS
-from nautobot_golden_config.utilities.helper import get_django_env
+from nautobot_golden_config.utilities.helper import get_django_env, render_jinja2
 
 
 def config_plan_default_status():
@@ -43,5 +43,4 @@ def generate_config_set_from_manual(device: Device, commands: str, context: dict
     if context is None:
         context = {}
     context.update({"obj": device})
-    template = rendering_engine.from_string(commands)
-    return "" + template.render(context=context)
+    return render_jinja2(template_code=commands, context=context, env=rendering_engine.env)

b13437fa3b49bb32173fda7d17f4eea0d791e770
bakebot <info@networktocode.com>
2026-03-10 16:32:44 +0000

    Cookie updated targeting develop by NetworkToCode Cookie Drift Manager Tool
    - [#350](https://github.com/nautobot/cookiecutter-nautobot-app/issues/350) - Added glightbox as a dependency to custom group docs in pyproject.toml files.
    
    **Full Changelog**: https://github.com/nautobot/cookiecutter-nautobot-app/compare/nautobot-app-v3.1.0...nautobot-app-v3.1.1

--- tasks.py
+++ tasks.py
@@ -283,31 +283,35 @@ def lock(context, check=False, constrain_nautobot_ver=False, constrain_python_ve
 # ------------------------------------------------------------------------------
 # START / STOP / DEBUG
 # ------------------------------------------------------------------------------
-@task(help={"service": "If specified, only affect this service."})
-def debug(context, service=""):
+@task(help={"service": "If specified, only affect the specified service(s); can be provided multiple times (i.e. -s nautobot -s worker)."}, iterable=["service"])
+def debug(context, service=None):
     """Start specified or all services and its dependencies in debug mode."""
-    print(f"Starting {service} in debug mode...")
+    service = " ".join(service) if service else ""
+    print(f"Starting {service or 'all services'} in debug mode...")
     docker_compose(context, "up", service=service)
 
 
-@task(help={"service": "If specified, only affect this service."})
-def start(context, service=""):
-    """Start specified or all services and its dependencies in detached mode."""
-    print("Starting Nautobot in detached mode...")
+@task(help={"service": "If specified, only affect the specified service(s); can be provided multiple times (i.e. -s nautobot -s worker)."}, iterable=["service"])
+def start(context, service=None):
+    """Start specified service(s) or all services and its dependencies in detached mode."""
+    service = " ".join(service) if service else ""
+    print(f"Starting {service or 'all services'} in detached mode...")
     docker_compose(context, "up --detach", service=service)
 
 
-@task(help={"service": "If specified, only affect this service."})
-def restart(context, service=""):
+@task(help={"service": "If specified, only affect the specified service(s); can be provided multiple times (i.e. -s nautobot -s worker)."}, iterable=["service"])
+def restart(context, service=None):
     """Gracefully restart specified or all services."""
-    print("Restarting Nautobot...")
+    service = " ".join(service) if service else ""
+    print(f"Restarting {service or 'all services'}...")
     docker_compose(context, "restart", service=service)
 
 
-@task(help={"service": "If specified, only affect this service."})
-def stop(context, service=""):
+@task(help={"service": "If specified, only affect the specified service(s); can be provided multiple times (i.e. -s nautobot -s worker)."}, iterable=["service"])
+def stop(context, service=None):
     """Stop specified or all services, if service is not specified, remove all containers."""
-    print("Stopping Nautobot...")
+    service = " ".join(service) if service else ""
+    print(f"Stopping {service or 'all services'}...")
     docker_compose(context, "stop" if service else "down --remove-orphans", service=service)
 
 
@@ -381,12 +385,13 @@ def vscode(context):
 
 @task(
     help={
-        "service": "If specified, only display logs for this service (default: all)",
+        "service": "If specified, only display logs for the specified service(s) (default: all); can be provided multiple times (i.e. -s nautobot -s worker)",
         "follow": "Flag to follow logs (default: False)",
         "tail": "Tail N number of lines (default: all)",
-    }
+    },
+    iterable=["service"],
 )
-def logs(context, service="", follow=False, tail=0):
+def logs(context, service=None, follow=False, tail=0):
     """View the logs of a docker compose service."""
     command = "logs "
 
@@ -394,6 +399,7 @@ def logs(context, service="", follow=False, tail=0):
         command += "--follow "
     if tail:
         command += f"--tail={tail} "
+    service = " ".join(service) if service else None
 
     docker_compose(context, command, service=service)
 
@@ -676,7 +682,10 @@ def backup_db(context, db_name="", output_file="dump.sql", readable=True):
 @task
 def docs(context):
     """Build and serve docs locally for development."""
-    command = "mkdocs serve -v"
+    # Note: explicitly enabling --livereload was added as a workaround
+    # and can be removed if/when mkdocs is updated.
+    # Ref: https://github.com/mkdocs/mkdocs/issues/4032
+    command = "mkdocs serve -v --livereload"
 
     if is_truthy(context.nautobot_golden_config.local):
         print(">>> Serving Documentation at http://localhost:8001")
@@ -717,7 +726,7 @@ def help_task(context):
 
 @task(
     help={
-        "version": "Version of Nautobot Dev Example App to generate the release notes for.",
+        "version": "Version of Golden Config to generate the release notes for.",
         "date": "Date of the release (default: today).",
         "keep": "Keep existing release notes files. Useful for testing. (default: False).",
     }

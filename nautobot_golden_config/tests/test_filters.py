fe0b211a12855478bd358af7c4a4eca11178a6ad
Joe Wesch <joe.wesch@networktocode.com>
2026-04-03 10:11:02 -0500

    Fixed isnull filter getting added to extra filters

--- nautobot_golden_config/tests/test_filters.py
+++ nautobot_golden_config/tests/test_filters.py
@@ -2,6 +2,7 @@
 
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
+from nautobot.apps.filters import MultiValueDateTimeFilter, MultiValueNumberFilter
 from nautobot.apps.testing import FilterTestCases
 from nautobot.dcim.models import Device, Platform
 from nautobot.extras.management import populate_role_choices, populate_status_choices
@@ -568,3 +569,20 @@ class ConfigPlanFilterTestCase(FilterTestCases.FilterTestCase):
         self.assertQuerysetEqualAndNotEmpty(
             filterset.qs, self.queryset.filter(plan_result_id=self.job_result1.id).distinct()
         )
+
+
+class GetFilterLookupDictTestCase(TestCase):
+    """Test that _get_filter_lookup_dict does not mutate shared lookup map constants."""
+
+    def test_isnull_not_added_to_numeric_filters(self):
+        """Verify that adding isnull for datetime filters does not leak into numeric filters."""
+
+        # First, call with a datetime filter to trigger the isnull addition
+        datetime_filter = MultiValueDateTimeFilter()
+        datetime_lookup = filters.GoldenConfigFilterSet._get_filter_lookup_dict(datetime_filter)
+        self.assertIn("isnull", datetime_lookup)
+
+        # Then verify a numeric filter does NOT get isnull
+        number_filter = MultiValueNumberFilter()
+        number_lookup = filters.GoldenConfigFilterSet._get_filter_lookup_dict(number_filter)
+        self.assertNotIn("isnull", number_lookup)
